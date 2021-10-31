cd56b2b9377430fed4b7ae6cae7ad2d638d3f16d
Mayya Markova <mayya.markova@gmail.com>
2021-10-31 20:09:52 +0200

    Add constants demo

---
+++ 01.packages, variables, functions/05.constants/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14 // Constants are declared like variables, but with the const keyword. They cannot be declared using the := syntax.
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
